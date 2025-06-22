26e1670a4e62f96826b7e276f61fa8368953f939
unknown <buttonsafrotech@gmail.com>
2025-06-22 04:47:05 +0100

    correct

--- app/Components/Nav.tsx
+++ app/Components/Nav.tsx
@@ -6,9 +6,9 @@ import hairways from '../../public/images/hairway.png'
 const Nav = () => {
   return (
     <>
-    <div className='dark:bg-gray-800  w-screen h-[150px] flex lg:gap-70 gap-19 bg-[#FFE1DE] border-b-2 justify-center items-center px-10 '>
+    <div className=' w-screen h-[150px] flex  md:gap-30 gap-19 bg-[#FFE1DE] border-b-2 justify-center items-center px-10 '>
         <Image src={logo} alt="" width={100} height={82} className='w-[100px] h-[82px]' />
-        <Image src={hairways} alt="" width={432.44} height={82} className='lg:w-[432.44px] h-[94px] sm:w-[242px] ' />
+        <Image src={hairways} alt="" width={432.44} height={82} className='lg:w-[432.44px] h-[94px] sm:w-[252px] ' />
        
         <button className='w-[100px] h-[52px] bg-[#FF6F61] rounded-tr-xl rounded-bl-xl font-extrabold text-[16px] text-center text-white'>
             &#8226; JOIN
